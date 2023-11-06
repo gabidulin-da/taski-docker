5407ec7ab0a2366c7d001ad4afb653cb3de23fc5
Denis Gabidulin <gabidulin.denis@yandex.ru>
2023-11-06 11:29:48 +0300

    flake

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -71,7 +70,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
-} 
+}
 
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
